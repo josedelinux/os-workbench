bb45c39ed108cfb31b2347e25c2b88a40721ac53
tracer-nju <tracer@nju.edu.cn>
2023-11-30 10:01:00 +0800

    Linux DESKTOP-HP-PLUS 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:01:00 up  1:07,  4 users,  load average: 0.04, 0.09, 0.08

---
+++ .shadow/pstree/pstree.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  int show_pids = false;
+  int numeric_sort = 0;
+  // for (int i = 0; i < argc; i++) {
+  //   assert(argv[i]);
+  //   printf("argv[%d] = %s\n", i, argv[i]);
+  // }
+  // assert(!argv[argc]);
+  int opt;
+  while ((opt = getopt(argc, argv, "pnV")) != -1) {
+    switch (opt) {
+      case 'p':
+        show_pids = true;
+        break;
+      case 'n':
+        numeric_sort = true;
+        break;
+      case 'V':
+        puts("pstree Version 0.1.0");
+        break;
+      default:
+        assert(0 && "Unknown option");
+        break;
+    }
+  }
+
+  return 0;
+}
